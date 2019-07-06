4cfc06ae147e1cbbf7c6ff7a5ee8d82ab9b9b908
royyan8589 <40899999+royyan8589@users.noreply.github.com>
2019-07-07 05:12:11 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="2018/07/xiaomi-redmi-5-plus.html"}
-if(x=="2"){xxx="2018/07/testing-xiaomi.html"}a_to_ve[i].href="https://www.kompisafelinkv2.ga/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="2018/07/testing-xiaomi.html"}a_to_ve[i].href="https://mr8589.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
